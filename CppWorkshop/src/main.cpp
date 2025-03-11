530403c1b9ee09bb64fe1ad550f972aa6513fd8d
Benjamin Caron <caron518@gmail.com>
2025-03-11 15:20:37 -0400

    Create main.cpp

---
+++ CppWorkshop/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "utils.h"
+
+int main() {
+    std::cout << "Welcome to the C++ Workshop!" << std::endl;
+    
+    int a = 5, b = 10;
+    std::cout << "Sum of " << a << " and " << b << " is: " << add(a, b) << std::endl;
+
+    return 0;
+}
